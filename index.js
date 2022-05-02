092f4f50cfab6358b80e6eedbe5b51c13130070a
naymarques <nay.marques@gmail.com>
2022-05-02 16:39:49 -0300

    iniciando a interação no JS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //quando o usuário escolhe cifra de cesar, abrir opção de incremento
 var decodeEscolhido = document.querySelector('#decodificador')
-var incremento = document.querySelector('#incremento')
+var incremento = document.querySelector('.incremento')
 decodeEscolhido.addEventListener('change', function () {
   if (decodeEscolhido.value == 'cifraCesar') {
     incremento.style = 'display: flex'
@@ -12,7 +12,7 @@ decodeEscolhido.addEventListener('change', function () {
 // quando o usuário decide entre codificar e decodifiar, muda o botão
 
 var tipoRadio = document.forms[0].escolhe
-var tipo = document.querySelector('.tipo')
+var tipo = document.querySelector('.escolheTraducao')
 var enviar = document.querySelector('.enviar')
 
 tipo.addEventListener('change', function () {
@@ -25,9 +25,52 @@ tipo.addEventListener('change', function () {
 
 // base 64
 
-var mensagem = document.querySelector('#input')
-var output = document.querySelector('#output')
-function traduz(mensagem) {
-  var codigo = btoa(mensagem)
-  resposta.innerText = codigo
+var formulario = document.forms.formulario
+
+formulario.addEventListener('submit', function (e) {
+  e.preventDefault()
+
+  var mensagem = formulario.input.value
+  var escolha = formulario.decodificador.value
+  var botoes = formulario.escolhe.value
+  var numeroIncremento = formulario.valorInc.value
+  var mensagemFinal = ''
+
+  if (escolha == 'cifraCesar') {
+    mensagemFinal = cesar(botoes, mensagem, numeroIncremento)
+  } else {
+    mensagemFinal = base64(botoes, mensagem)
+  }
+
+  var resultadoTexto = document.getElementById('output')
+  resultadoTexto.innerHTML = `${mensagemFinal}`
+})
+
+/* Cifra */
+function cesar(escolhe, mensagem, numeroIncremento) {
+  numeroIncremento = Number(numeroIncremento)
+
+  var mensagemFinal = ''
+
+  for (var i = 0; i < mensagem.length; i++) {
+    var letra = mensagem[i]
+    var codigo = mensagem.charCodeAt()
+
+    if (escolhe == 'codificar') {
+      codigo += numeroIncremento
+    } else {
+      codigo -= numeroIncremento
+    }
+    mensagemFinal += String.fromCharCode(codigo)
+  }
+  return mensagemFinal
+}
+
+/* Base */
+function base64(escolhe, mensagem) {
+  if (escolhe == 'codificar') {
+    return btoa(mensagem)
+  } else {
+    return atob(mensagem)
+  }
 }
